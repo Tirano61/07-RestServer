5ff17c1d8b1d2f844486fdac48e31e8bc4767a8a
Dario <dramirez@balanzashook.com.ar>
2021-04-01 11:43:00 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,27 @@
 //=====================
 //  Puerto
 //=====================
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
+
+//=====================
+//  Entorno
+//=====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//=====================
+//  Base de datos
+//=====================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://usuario:QPDn4Bt8939a9R7x@cluster0.rygsb.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
+
+
+// usuario
+// QPDn4Bt8939a9R7x
+// MongoDB URL
+//mongodb+srv://usuario:QPDn4Bt8939a9R7x@cluster0.rygsb.mongodb.net/cafe
